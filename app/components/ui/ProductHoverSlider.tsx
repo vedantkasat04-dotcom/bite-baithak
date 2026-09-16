a3d481181e2fee551c2018df9c3248a842938534
Vedant Kasat <vedant.kasat04@gmail.com>
2026-09-16 14:09:26 +0530

    hover speed 1200ms

--- app/components/ui/ProductHoverSlider.tsx
+++ app/components/ui/ProductHoverSlider.tsx
@@ -27,7 +27,7 @@ export default function ProductHoverSlider({
     if (!hovering || images.length < 2) return
     const id = setInterval(() => {
       setActive((i) => (i + 1) % images.length)
-    }, 1400)
+    }, 1200)
     return () => clearInterval(id)
   }, [hovering, images.length])
 
